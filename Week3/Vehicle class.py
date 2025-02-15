4ef9b04667ae270c2bd0c153823a4a7e22078e0a
AlBloom <156461814+AlBloom@users.noreply.github.com>
2025-02-15 14:53:46 +0300

    Create Vehicle class.py

---
+++ Week3/Vehicle class.py
@@ -0,0 +1,37 @@
+class Vehicle:
+  def __init__(self, make, model):
+    self._make = make
+    self._model = model
+
+  def describe(self):
+    return f"{self._make} {self._model}"
+
+
+class Car(Vehicle):
+  def __init__(self, make, model, num_doors):
+    super().__init__(make, model)
+    self._num_doors = num_doors
+
+  def describe(self):
+    return f"{self._make} {self._model} with {self._num_doors} doors"
+
+
+class Bike(Vehicle):
+  def __init__(self, make, model, bike_type):
+    super().__init__(make, model)
+    self._bike_type = bike_type
+
+  def describe(self):
+    return f"{self._make} {self._model} ({self._bike_type})"
+
+
+def display_description(vehicle):
+  print(vehicle.describe())
+
+
+if __name__ == "__main__":
+    car = Car("Toyota", "Corolla", 4)
+    bike = Bike("Trek", "Marlin 5", "Mountain Bike")
+
+    display_description(car)
+    display_description(bike)
